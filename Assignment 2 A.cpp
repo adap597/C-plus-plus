feb78b0189a7a658574fa0561592c5726cc630a8
Aurora <82787709+adap597@users.noreply.github.com>
2021-05-03 14:10:40 -0700

    Update Assignment 2 A.cpp

--- Assignment 2 A.cpp
+++ Assignment 2 A.cpp
@@ -1,9 +1,21 @@
+/*Create three arrays of 20 integers each. Fill in two arrays with data, leaving the third blank. 
+From main, pass these three arrays into a function. Inside the function, add the value of each array element and store it in the third.
+
+third_array[i] = second_array[i] + first_array[i]
+
+Back in main, print out the value of every element in the third/sum array. Excellent output on this assignment might show something like:
+
+
+Array 1		+	Array 2		=	Array 3
+5		+	3		=	8
+10		+	4		=	14
+
+*/
+
 // Assignment 2 A.cpp : A program that accepts user input into 2 arrays. It then adds the value of each array element
 //from array 1 and array 2 and stores the sum in the third array. It then displays a table with the values of each element
 //from each array and the sums. 
 
-//Name: Aurora DiPeso
-//University ID: 0663597
 //Thomas Edison State University
 //C++ Programming (COS-213)
 //Section no. OL009

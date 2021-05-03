92a18eb89e1107be44d8458c2ba7ce51a9369147
Aurora <82787709+adap597@users.noreply.github.com>
2021-05-03 14:20:56 -0700

    Update Comprehensive 3.cpp

--- Comprehensive 3.cpp
+++ Comprehensive 3.cpp
@@ -1,7 +1,18 @@
+/*Define a class called Revenue that has 2 private members of demand and price. 
+Include a constructor and a public function that calculates the RevenueValue of the class.
+
+RevenueValue = demand * price
+
+Use public functions of the class in a program that computes RevenueValue and displays RevenueValue when demand is 2000 and price is 2.
+ 
+Output should be displayed as the following: “The Revenue Value for the current income source with a price of xyz and a market value 
+demand of demand-based-assessment is currently abc.”   
+ 
+Where xyz is the price entered and demand-based-assessment is the current market value (in this case 2000), and finally abc as the Revenue Value.
+*/
+
 // Comprehensive 3.cpp : This program tests the Revenue class.
 //
-//Name: Aurora DiPeso
-//University ID: 0663597
 //Thomas Edison State University
 //C++ Programming (COS-213)
 //Section no. OL009
